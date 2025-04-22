637ed8e1f2adeed2a700e6a3e748c39ff6749f1f
Maytê <maytearaujo@yahoo.com.br>
2025-04-22 00:56:46 -0300

    feat: Fouvorite

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Fovourite from "./components/fovourite/Fovourite"
 import Hero from "./components/hero/Hero"
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
       <Hero />
+      <Fovourite />
     </>
   )
 }
