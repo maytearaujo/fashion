3c88e76a813ab1d996509698218bd4d2f733a480
Maytê <maytearaujo@yahoo.com.br>
2026-04-14 12:45:34 -0300

    fix: remove border

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -2,12 +2,12 @@ import MulherBlusaAmarela from "../../assets/images/banner/mulher_blusa_amarela.
 const Banner = () => {
   return (
     <>
-      <div className="bg-amarelo flex h-screen border border-4 border-amber-950">
+      <div className="bg-amarelo flex h-screen">
         <div className="w-[50%]">
           <img src={MulherBlusaAmarela} alt="Mulher com Blusa Amarela" />
         </div>
 
-        <div className="w-[100%] border border-4 border-blue-950">
+        <div className="w-[100%] p-5">
           <div className="h-[80%] ">
             <h3 className="font-black text-8xl">
               <span className="bg-branco">
